ad89aed76dc9723a9cdc61a1adf22b23e0b36441
veyzz <munir.taga@gmail.com>
2022-04-09 14:11:36 +0300

    add integer register-immediate instructions

--- src/hart.c
+++ src/hart.c
@@ -90,6 +90,81 @@ int fetch(uint8_t *ip)
       }
       break;
 
+    case 0b0010011:
+
+      switch(GET_FUNCT3(instr))
+      {
+        case 0x0:
+          reg_gp[GET_RD(instr)] = \
+            reg_gp[GET_RS1(instr)] + GET_IMM_I(instr);
+          break;
+
+        case 0x4:
+          reg_gp[GET_RD(instr)] = \
+            reg_gp[GET_RS1(instr)] ^ GET_IMM_I(instr);
+          break;
+
+        case 0x6:
+          reg_gp[GET_RD(instr)] = \
+            reg_gp[GET_RS1(instr)] | GET_IMM_I(instr);
+          break;
+
+        case 0x7:
+          reg_gp[GET_RD(instr)] = \
+            reg_gp[GET_RS1(instr)] & GET_IMM_I(instr);
+          break;
+
+        case 0x1:
+
+          switch(GET_BIT_FIELD(GET_IMM_I(instr), 5, 11))
+          {
+
+            case 0x00:
+              reg_gp[GET_RD(instr)] = \
+                reg_gp[GET_RS1(instr)] << GET_BIT_FIELD(GET_IMM_I(instr), 0, 4);
+              break;
+
+            default:
+              return 1;
+          }
+          break;
+
+        case 0x5:
+
+          switch(GET_BIT_FIELD(GET_IMM_I(instr), 5, 11))
+          {
+
+            case 0x00:
+              reg_gp[GET_RD(instr)] = \
+                reg_gp[GET_RS1(instr)] >> GET_BIT_FIELD(GET_IMM_I(instr), 0, 4);
+              break;
+
+            case 0x20:
+              reg_gp[GET_RD(instr)] = \
+                (uint32_t)((int32_t)reg_gp[GET_RS1(instr)] >> \
+                           (int32_t)GET_BIT_FIELD(GET_IMM_I(instr), 0, 4));
+              break;
+
+            default:
+              return 1;
+          }
+          break;
+
+        case 0x2:
+          reg_gp[GET_RD(instr)] = \
+            ((int32_t)reg_gp[GET_RS1(instr)] < (int32_t)GET_IMM_I(instr)) ? 1 : 0;
+          break;
+
+        case 0x3:
+          reg_gp[GET_RD(instr)] = \
+            (reg_gp[GET_RS1(instr)] < GET_IMM_I(instr)) ? 1 : 0;
+          break;
+
+        default:
+          return 1;
+      }
+      break;
+
     default:
       return 1;
   }
