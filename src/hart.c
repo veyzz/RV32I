652e3ed699a7c580a96b654de3532eb3552da424
veyzz <munir.taga@gmail.com>
2026-04-28 23:43:03 +0300

    hart: fix bugs and refactor fetch code
    * Control Transfer Instructions: avoid wrong PC step after branches and jumps
    * LUI/AUIPC: use U-type immediate without double shift
    * Replace opcode/funct field literals with RV_* macros

--- src/hart.c
+++ src/hart.c
@@ -9,23 +9,26 @@ int fetch(uint8_t *ip)
 
   reg_gp[REG_X0] = 0;
 
+  uint32_t const pc_cur = reg_pc[0];
+  uint32_t pc_next = pc_cur + sizeof(uint32_t);
+
   switch (GET_OPCODE(instr))
   {
-    case 0b0110011:
+    case RV_OP_OP:
 
-      switch(GET_FUNCT3(instr))
+      switch (GET_FUNCT3(instr))
       {
-        case 0x0:
+        case RV_F3_ADD_SUB:
 
-          switch(GET_FUNCT7(instr))
+          switch (GET_FUNCT7(instr))
           {
 
-            case 0x00:
+            case RV_F7_ADD_SRL:
               reg_gp[GET_RD(instr)] = \
                 reg_gp[GET_RS1(instr)] + reg_gp[GET_RS2(instr)];
               break;
 
-            case 0x20:
+            case RV_F7_SUB_SRA:
               reg_gp[GET_RD(instr)] = \
                 reg_gp[GET_RS1(instr)] - reg_gp[GET_RS2(instr)];
               break;
@@ -35,37 +38,37 @@ int fetch(uint8_t *ip)
           }
           break;
 
-        case 0x4:
+        case RV_F3_XOR:
           reg_gp[GET_RD(instr)] = \
             reg_gp[GET_RS1(instr)] ^ reg_gp[GET_RS2(instr)];
           break;
 
-        case 0x6:
+        case RV_F3_OR:
           reg_gp[GET_RD(instr)] = \
             reg_gp[GET_RS1(instr)] | reg_gp[GET_RS2(instr)];
           break;
 
-        case 0x7:
+        case RV_F3_AND:
           reg_gp[GET_RD(instr)] = \
             reg_gp[GET_RS1(instr)] & reg_gp[GET_RS2(instr)];
           break;
 
-        case 0x1:
+        case RV_F3_SLL:
           reg_gp[GET_RD(instr)] = \
             reg_gp[GET_RS1(instr)] << reg_gp[GET_RS2(instr)];
           break;
 
-        case 0x5:
+        case RV_F3_SRL_SRA:
 
-          switch(GET_FUNCT7(instr))
+          switch (GET_FUNCT7(instr))
           {
 
-            case 0x00:
+            case RV_F7_ADD_SRL:
               reg_gp[GET_RD(instr)] = \
                 reg_gp[GET_RS1(instr)] >> reg_gp[GET_RS2(instr)];
               break;
 
-            case 0x20:
+            case RV_F7_SUB_SRA:
               reg_gp[GET_RD(instr)] = \
                 (uint32_t)((int32_t)reg_gp[GET_RS1(instr)] >> (int32_t)reg_gp[GET_RS2(instr)]);
               break;
@@ -75,12 +78,12 @@ int fetch(uint8_t *ip)
           }
           break;
 
-        case 0x2:
+        case RV_F3_SLT:
           reg_gp[GET_RD(instr)] = \
             ((int32_t)reg_gp[GET_RS1(instr)] < (int32_t)reg_gp[GET_RS2(instr)]) ? 1 : 0;
           break;
 
-        case 0x3:
+        case RV_F3_SLTU:
           reg_gp[GET_RD(instr)] = \
             (reg_gp[GET_RS1(instr)] < reg_gp[GET_RS2(instr)]) ? 1 : 0;
           break;
@@ -90,36 +93,36 @@ int fetch(uint8_t *ip)
       }
       break;
 
-    case 0b0010011:
+    case RV_OP_IMM:
 
-      switch(GET_FUNCT3(instr))
+      switch (GET_FUNCT3(instr))
       {
-        case 0x0:
+        case RV_F3_ADD_SUB:
           reg_gp[GET_RD(instr)] = \
             reg_gp[GET_RS1(instr)] + GET_IMM_I(instr);
           break;
 
-        case 0x4:
+        case RV_F3_XOR:
           reg_gp[GET_RD(instr)] = \
             reg_gp[GET_RS1(instr)] ^ GET_IMM_I(instr);
           break;
 
-        case 0x6:
+        case RV_F3_OR:
           reg_gp[GET_RD(instr)] = \
             reg_gp[GET_RS1(instr)] | GET_IMM_I(instr);
           break;
 
-        case 0x7:
+        case RV_F3_AND:
           reg_gp[GET_RD(instr)] = \
             reg_gp[GET_RS1(instr)] & GET_IMM_I(instr);
           break;
 
-        case 0x1:
+        case RV_F3_SLL:
 
-          switch(GET_BIT_FIELD(GET_IMM_I(instr), 5, 11))
+          switch (GET_BIT_FIELD(GET_IMM_I(instr), 5, 11))
           {
 
-            case 0x00:
+            case RV_F7_ADD_SRL:
               reg_gp[GET_RD(instr)] = \
                 reg_gp[GET_RS1(instr)] << GET_BIT_FIELD(GET_IMM_I(instr), 0, 4);
               break;
@@ -129,17 +132,17 @@ int fetch(uint8_t *ip)
           }
           break;
 
-        case 0x5:
+        case RV_F3_SRL_SRA:
 
-          switch(GET_BIT_FIELD(GET_IMM_I(instr), 5, 11))
+          switch (GET_BIT_FIELD(GET_IMM_I(instr), 5, 11))
           {
 
-            case 0x00:
+            case RV_F7_ADD_SRL:
               reg_gp[GET_RD(instr)] = \
                 reg_gp[GET_RS1(instr)] >> GET_BIT_FIELD(GET_IMM_I(instr), 0, 4);
               break;
 
-            case 0x20:
+            case RV_F7_SUB_SRA:
               reg_gp[GET_RD(instr)] = \
                 (uint32_t)((int32_t)reg_gp[GET_RS1(instr)] >> \
                            (int32_t)GET_BIT_FIELD(GET_IMM_I(instr), 0, 4));
@@ -150,12 +153,12 @@ int fetch(uint8_t *ip)
           }
           break;
 
-        case 0x2:
+        case RV_F3_SLT:
           reg_gp[GET_RD(instr)] = \
             ((int32_t)reg_gp[GET_RS1(instr)] < (int32_t)GET_IMM_I(instr)) ? 1 : 0;
           break;
 
-        case 0x3:
+        case RV_F3_SLTU:
           reg_gp[GET_RD(instr)] = \
             (reg_gp[GET_RS1(instr)] < GET_IMM_I(instr)) ? 1 : 0;
           break;
@@ -165,35 +168,35 @@ int fetch(uint8_t *ip)
       }
       break;
 
-    case 0b0000011:
+    case RV_OP_LOAD:
 
-      switch(GET_FUNCT3(instr))
+      switch (GET_FUNCT3(instr))
       {
-        case 0x0:
+        case RV_F3_MEM_B:
           reg_gp[GET_RD(instr)] = \
             *(int8_t *) \
              (memory + (reg_gp[GET_RS1(instr)] + GET_IMM_I(instr)) % MEMSIZE);
           break;
 
-        case 0x1:
+        case RV_F3_MEM_H:
           reg_gp[GET_RD(instr)] = \
             *(int16_t *) \
              (memory + (reg_gp[GET_RS1(instr)] + GET_IMM_I(instr)) % MEMSIZE);
           break;
 
-        case 0x2:
+        case RV_F3_MEM_W:
           reg_gp[GET_RD(instr)] = \
             *(uint32_t *) \
              (memory + (reg_gp[GET_RS1(instr)] + GET_IMM_I(instr)) % MEMSIZE);
           break;
 
-        case 0x4:
+        case RV_F3_MEM_BU:
           reg_gp[GET_RD(instr)] = \
             *(uint8_t *) \
              (memory + (reg_gp[GET_RS1(instr)] + GET_IMM_I(instr)) % MEMSIZE);
           break;
 
-        case 0x5:
+        case RV_F3_MEM_HU:
           reg_gp[GET_RD(instr)] = \
             *(uint16_t *) \
              (memory + (reg_gp[GET_RS1(instr)] + GET_IMM_I(instr)) % MEMSIZE);
@@ -204,23 +207,23 @@ int fetch(uint8_t *ip)
       }
       break;
 
-    case 0b0100011:
+    case RV_OP_STORE:
 
-      switch(GET_FUNCT3(instr))
+      switch (GET_FUNCT3(instr))
       {
-        case 0x0:
+        case RV_F3_MEM_B:
           memcpy(memory + (reg_gp[GET_RS1(instr)] + GET_IMM_S(instr)) % MEMSIZE,
                  reg_gp + GET_RS2(instr),
                  sizeof(uint8_t));
           break;
 
-        case 0x1:
+        case RV_F3_MEM_H:
           memcpy(memory + (reg_gp[GET_RS1(instr)] + GET_IMM_S(instr)) % MEMSIZE,
                  reg_gp + GET_RS2(instr),
                  sizeof(uint16_t));
           break;
 
-        case 0x2:
+        case RV_F3_MEM_W:
           memcpy(memory + (reg_gp[GET_RS1(instr)] + GET_IMM_S(instr)) % MEMSIZE,
                  reg_gp + GET_RS2(instr),
                  sizeof(uint32_t));
@@ -231,49 +234,49 @@ int fetch(uint8_t *ip)
       }
       break;
 
-    case 0b1100011:
+    case RV_OP_BRANCH:
 
-      switch(GET_FUNCT3(instr))
+      switch (GET_FUNCT3(instr))
       {
-        case 0x0:
+        case RV_F3_BR_BEQ:
           if (reg_gp[GET_RS1(instr)] == reg_gp[GET_RS2(instr)])
           {
-            reg_pc[0] += GET_IMM_B(instr);
+            pc_next = pc_cur + GET_IMM_B(instr);
           }
           break;
 
-        case 0x1:
+        case RV_F3_BR_BNE:
           if (reg_gp[GET_RS1(instr)] != reg_gp[GET_RS2(instr)])
           {
-            reg_pc[0] += GET_IMM_B(instr);
+            pc_next = pc_cur + GET_IMM_B(instr);
           }
           break;
 
-        case 0x4:
+        case RV_F3_BR_BLT:
           if ((int32_t)reg_gp[GET_RS1(instr)] < (int32_t)reg_gp[GET_RS2(instr)])
           {
-            reg_pc[0] += GET_IMM_B(instr);
+            pc_next = pc_cur + GET_IMM_B(instr);
           }
           break;
 
-        case 0x5:
+        case RV_F3_BR_BGE:
           if ((int32_t)reg_gp[GET_RS1(instr)] >= (int32_t)reg_gp[GET_RS2(instr)])
           {
-            reg_pc[0] += GET_IMM_B(instr);
+            pc_next = pc_cur + GET_IMM_B(instr);
           }
           break;
 
-        case 0x6:
+        case RV_F3_BR_BLTU:
           if (reg_gp[GET_RS1(instr)] < reg_gp[GET_RS2(instr)])
           {
-            reg_pc[0] += GET_IMM_B(instr);
+            pc_next = pc_cur + GET_IMM_B(instr);
           }
           break;
 
-        case 0x7:
+        case RV_F3_BR_BGEU:
           if (reg_gp[GET_RS1(instr)] >= reg_gp[GET_RS2(instr)])
           {
-            reg_pc[0] += GET_IMM_B(instr);
+            pc_next = pc_cur + GET_IMM_B(instr);
           }
           break;
 
@@ -282,19 +285,19 @@ int fetch(uint8_t *ip)
       }
       break;
 
-    case 0b1101111:
+    case RV_OP_JAL:
 
-      reg_gp[GET_RD(instr)] = reg_pc[0] + sizeof(uint32_t);
-      reg_pc[0] += GET_IMM_J(instr);
+      reg_gp[GET_RD(instr)] = pc_cur + sizeof(uint32_t);
+      pc_next = pc_cur + GET_IMM_J(instr);
       break;
 
-    case 0b1100111:
+    case RV_OP_JALR:
 
-      switch(GET_FUNCT3(instr))
+      switch (GET_FUNCT3(instr))
       {
-        case 0x0:
-          reg_gp[GET_RD(instr)] = reg_pc[0] + sizeof(uint32_t);
-          reg_pc[0] = reg_gp[GET_RS1(instr)] + GET_IMM_I(instr);
+        case RV_F3_JALR:
+          reg_gp[GET_RD(instr)] = pc_cur + sizeof(uint32_t);
+          pc_next = reg_gp[GET_RS1(instr)] + GET_IMM_I(instr);
           break;
 
         default:
@@ -302,22 +305,21 @@ int fetch(uint8_t *ip)
       }
       break;
 
-    case 0b0110111:
+    case RV_OP_LUI:
 
-      reg_gp[GET_RD(instr)] = GET_IMM_U(instr) << 12;
+      reg_gp[GET_RD(instr)] = GET_IMM_U(instr);
       break;
 
-    case 0b0010111:
+    case RV_OP_AUIPC:
 
-      reg_gp[GET_RD(instr)] = reg_pc[0] + (GET_IMM_U(instr) << 12);
+      reg_gp[GET_RD(instr)] = pc_cur + GET_IMM_U(instr);
       break;
 
     default:
       return 1;
   }
 
-  reg_pc[0] += sizeof(uint32_t);
+  reg_pc[0] = pc_next;
 
   return 0;
 }
-
